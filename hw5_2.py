9957d52a3d9b61d02304780b3af570cea77bdf40
newxata <9119355@gmail.com>
2025-07-20 00:01:58 +0300

    second commit

---
+++ hw5_2.py
@@ -0,0 +1,15 @@
+import re
+from typing import Callable
+
+def generator_numbers(text: str):
+    list_numbers = re.findall(r'\d+\.\d+', text)
+    for numbers in list_numbers:
+        yield float(numbers)
+
+def sum_profit(text: str, func: Callable):
+    total_sum = sum(func(text))
+    return total_sum
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
